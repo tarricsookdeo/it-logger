4a4ef6fb76d1375146da3d69b430c6f0b304547b
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-04-21 19:46:53 -0400

    Initial project setup and cleanup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+
+  return (
+    <div className='App'>
+      <h1>My App</h1>
+    </div>
+  );
+};
+
+export default App;
