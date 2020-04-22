3377c8b277f8414e9d709e81e29afb50448fbcda
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-04-21 20:25:42 -0400

    Added SearchBar and Logs components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import './App.css';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
   useEffect(() => {
@@ -11,7 +12,10 @@ const App = () => {
 
   return (
     <div className='App'>
-      <h1>My App</h1>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
     </div>
   );
 };
