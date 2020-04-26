eb252ce350de797bea6bdfe9a66ae5cc2e8dd1ed
github username <github email address>
2020-04-26 02:59:17 -0400

    Finish UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 const App = () => {
   useEffect(() => {
@@ -22,6 +23,7 @@ const App = () => {
         <AddLogModal />
         <EditLogModal />
         <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </div>
