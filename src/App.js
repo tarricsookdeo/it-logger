989ae0198c7dd7afdbbe38973d9066b7c025b2a1
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-04-25 23:13:59 -0400

    Start adding TechItem components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
 
 const App = () => {
   useEffect(() => {
@@ -18,6 +20,8 @@ const App = () => {
       <div className='container'>
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
         <Logs />
       </div>
     </div>
