f7f928c2a2eee9aae12db90d49f7a887e2bd6d73
github username <github email address>
2020-04-26 03:23:38 -0400

    Setup Redux boilerplate.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   useEffect(() => {
@@ -16,17 +18,19 @@ const App = () => {
   });
 
   return (
-    <div className='App'>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
+    <Provider store={store}>
+      <div className='App'>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
