66900bba47e23185152136b312b9ada46ca340ea
github username <github email address>
2020-04-26 07:38:02 -0400

    Complete log functionality

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { searchLogs } from '../../actions/logActions';
 
-const SearchBar = () => {
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef('');
+
+  const onChange = (e) => {
+    searchLogs(text.current.value);
+  };
+
   return (
     <div>
       <nav style={{ marginBottom: '30px' }} className='blue z-depth-0'>
         <div className='nav-wrapper'>
           <form>
             <div className='input-field'>
-              <input id='search' type='search' />
+              <input
+                id='search'
+                type='search'
+                placeholder='Search Logs...'
+                ref={text}
+                onChange={onChange}
+              />
               <label className='label-icon' htmlFor='search'>
                 <i className='material-icons'>search</i>
               </label>
@@ -20,4 +35,8 @@ const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired,
+};
+
+export default connect(null, { searchLogs })(SearchBar);
